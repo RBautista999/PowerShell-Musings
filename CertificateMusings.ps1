a598381f15c5b6679bd1f0b153449b1684f5b8f4
RBautista999 <robert.bautista999@mgail.com>
2022-09-03 17:37:38 -0700

    Cleaned up formatting

--- CertificateMusings.ps1
+++ CertificateMusings.ps1
@@ -8,28 +8,27 @@ if (!(Get-Module -Name ActiveDirectory)) {
 
 $Computers = (Get-ADComputer -LDAPFilter "(&(objectCategory=computer)(operatingSystem=Windows 10*))").DNSHostName
 
-$results = $Computers | Foreach-Object -ThrottleLimit 10 -Parallel {
+$Results = $Computers | Foreach-Object -ThrottleLimit 10 -Parallel {
     if (Test-Connection -TargetName $PSItem -Quiet) {
         Write-Host "Connection to $($PSItem) successful" -ForegroundColor Green 
         $Parameters = @{
             ComputerName = $PSItem
             ScriptBlock  = { Get-ChildItem $args | Where-Object { $_.Subject -like "*Lightspeed*" } }
             ArgumentList = "Cert:\LocalMachine\Root\"
-            
         }
         
         $GetCert = Invoke-Command @Parameters
         if ($null -eq $GetCert) {
-            Write-Host "!!!$($PSItem) doesn't have Lightspeed Relay Smart Agent installed!!!" -ForegroundColor Red
+            Write-Warning "$($PSItem) doesn't have Lightspeed Relay Smart Agent installed"
         }
         else {
             Write-Host "--->We found a Lightspeed cert, adding result(s) to array<---" -ForegroundColor Cyan
             foreach ($Cert in $GetCert) {
                 [PSCustomObject]@{
-                    'Computer'    = $PSItem;
-                    'Certificate' = $Cert.Issuer;
+                    'Computer'    = $PSItem
+                    'Certificate' = $Cert.Issuer
                     'Begins'      = $Cert.GetEffectiveDateString()
-                    'Expires'     = $Cert.GetExpirationDateString();
+                    'Expires'     = $Cert.GetExpirationDateString()
                 }
             }
         }
@@ -40,4 +39,4 @@ $results = $Computers | Foreach-Object -ThrottleLimit 10 -Parallel {
     }
 }
 
-$results | Sort-Object { $PSItem.Expires -as [datetime] }  | Export-CSV -NoTypeInformation 'LightspeedCerts.csv'
+$Results | Sort-Object { $PSItem.Expires -as [datetime] }  | Export-CSV -NoTypeInformation 'LightspeedCerts.csv'
