a0cf4be1afffba11f6ff71d88f4d62dbcb8cb478
RBautista999 <robert.bautista999@mgail.com>
2022-09-03 17:02:08 -0700

    Implemented parallel processing to speed up script

--- CertificateMusings.ps1
+++ CertificateMusings.ps1
@@ -1,19 +1,32 @@
+$ErrorActionPreference = "Stop"
+#requires -version 7.2
 
-$Computers = (Get-ADComputer -LDAPFilter "(&(objectCategory=computer)(operatingSystem=Windows 10*))").Name
-$result = @()
-$Result = foreach ($Computer in $Computers) {
-    $ErrorActionPreference = "SilentlyContinue"
-    if (Test-Connection -TargetName $Computer -Quiet) {
-        Write-Host "Connection to $($Computer) succesful" -ForegroundColor Green 
-        $GetCert = Invoke-Command -ComputerName $Computer { Get-ChildItem Cert:\LocalMachine\Root\ | Where-Object { $_.Subject -like "*Lightspeed*" } }
+if (!(Get-Module -Name ActiveDirectory)) {
+    Write-Output "Importing Active Directory Module"
+    Import-Module ActiveDirectory
+}
+
+$Computers = (Get-ADComputer -LDAPFilter "(&(objectCategory=computer)(operatingSystem=Windows 10*))").DNSHostName
+
+$results = $Computers | Foreach-Object -ThrottleLimit 10 -Parallel {
+    if (Test-Connection -TargetName $PSItem -Quiet) {
+        Write-Host "Connection to $($PSItem) successful" -ForegroundColor Green 
+        $Parameters = @{
+            ComputerName = $PSItem
+            ScriptBlock  = { Get-ChildItem $args | Where-Object { $_.Subject -like "*Lightspeed*" } }
+            ArgumentList = "Cert:\LocalMachine\Root\"
+            
+        }
+        
+        $GetCert = Invoke-Command @Parameters
         if ($null -eq $GetCert) {
-            Write-Host "!!!$($Computer) doesn't have Lightspeed Relay Smart Agent installed!!!" -ForegroundColor Red
+            Write-Host "!!!$($PSItem) doesn't have Lightspeed Relay Smart Agent installed!!!" -ForegroundColor Red
         }
         else {
             Write-Host "--->We found a Lightspeed cert, adding result(s) to array<---" -ForegroundColor Cyan
             foreach ($Cert in $GetCert) {
                 [PSCustomObject]@{
-                    'Computer'    = $Computer;
+                    'Computer'    = $PSItem;
                     'Certificate' = $Cert.Issuer;
                     'Begins'      = $Cert.GetEffectiveDateString()
                     'Expires'     = $Cert.GetExpirationDateString();
@@ -23,9 +36,8 @@ $Result = foreach ($Computer in $Computers) {
         
     }
     else {
-        Write-Warning -Message "Unable to connect to $($Computer)"
+        Write-Warning -Message "Unable to connect to $($PSItem)"
     }
-    
-
 }
-$Result | Sort-Object Expires | Export-CSV -NoTypeInformation 'LightspeedCerts.csv'
+
+$results | Sort-Object { $PSItem.Expires -as [datetime] }  | Export-CSV -NoTypeInformation 'LightspeedCerts.csv'
