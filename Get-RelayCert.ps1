81a7ee09cbd5651964a44e06ee3fe61262668a70
RBautista999 <robert.bautista999@mgail.com>
2022-09-07 19:45:08 -0700

    Need to finish up the remote portion

--- Get-RelayCert.ps1
+++ Get-RelayCert.ps1
@@ -6,6 +6,8 @@ function Get-RelayCert {
         Retrives Relay Smart Agent/Filter Certificate
     .DESCRIPTION
         Use this to determine whether Relay's cert has expired
+    .PARAMETER ComputerName
+
     .PARAMETER Path
         Used to define path to Certificate Store( if different from default )
     .PARAMETER IncludeDates
@@ -18,11 +20,15 @@ function Get-RelayCert {
         This will display results with 'Issuer, Start Date, and Expiration Date'
     #>
     [CmdletBinding()]
-    [OutputType('System.Security.Cryptography.X509Certificates.X509Certificate2')]
     param (
         [Parameter(Mandatory = $False,
-            ValueFromPipeline = $False,
+            ValueFromPipeline = $True,
             Position = 0)]
+        [string]
+        $ComputerName,
+        [Parameter(Mandatory = $False,
+            ValueFromPipeline = $False,
+            Position = 1)]
         [string]
         $Path = "Cert:\LocalMachine\Root\",
         [Parameter(Mandatory = $False,
@@ -30,16 +36,54 @@ function Get-RelayCert {
         [switch]
         $IncludeDates    
     )
-    $Cert = Get-ChildItem $Path | Where-Object { $_.Subject -like "*Lightspeed*" }
-    if ($IncludeDates) {
+    begin {
         $ObjectParams = @{
             Property = "Issuer",
             @{Name = "Start Date"; Expression = { $_.NotBefore } },
             @{Name = "Expiration Date"; Expression = { $_.NotAfter } }
         }
-        $Cert | Select-Object @ObjectParams
+    
     }
-    else {
-        $Cert
-    }    
+    process {
+        if ($ComputerName) {
+            $Results = $ComputerName | Foreach-Object -ThrottleLimit 10 -Parallel {
+                if (Test-Connection -TargetName $PSItem -Quiet) {
+                    Write-Output "Connection to $($PSItem) successful"
+                    $RemoteParams = @{
+                        ComputerName = $PSItem
+                        ScriptBlock  = { Get-ChildItem $args | Where-Object { $_.Subject -like "*Lightspeed*" } }
+                        ArgumentList = "Cert:\LocalMachine\Root\"
+                    }
+                    
+                    $GetCert = Invoke-Command @RemoteParams
+                    if ($null -eq $GetCert) {
+                        Write-Warning "$($PSItem) doesn't have Lightspeed Relay Smart Agent installed"
+                    }
+                    else {
+                        Write-Host "Adding resuts to array" 
+                        foreach ($Cert in $GetCert) {
+                            [PSCustomObject]@{
+                                'Computer'    = $PSItem
+                                'Certificate' = $Cert.Issuer
+                                'Begins'      = $Cert.GetEffectiveDateString()
+                                'Expires'     = $Cert.GetExpirationDateString()
+                            }
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+        else {
+            $Cert = Get-ChildItem $Path | Where-Object { $_.Subject -like "*Lightspeed*" }
+            if ($IncludeDates) {
+                
+                $Cert | Select-Object @ObjectParams
+            }
+            else {
+                $Cert
+            }
+        }    
+    }      
 }   
